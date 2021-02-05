49a544cf285d5c47c2d13ef1c8356cd0f4564a50
Santos Pierre <santospierre1305@gmail.com>
2021-02-05 13:19:21 +0100

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+            <div className="text-center">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <span className="block xl:inline">Hello World</span>
+                </h1>
+            </div>
+        </main>
+    );
+}
