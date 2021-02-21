1b15d8a1bb1b2346f5ae89598ce13270af43ebf7
Pierre Santos <santospierre1305@gmail.com>
2021-02-21 20:23:58 +0100

    Action Menu Note Element (wip)

--- components/Sidebar/Items/NoteElement.tsx
+++ components/Sidebar/Items/NoteElement.tsx
@@ -3,7 +3,8 @@ import { getSettings } from '@/redux/selectors';
 import { updateActiveFolder, updateActiveNote } from '@/redux/slices/dataSlice';
 import { updateUserSelection } from '@/redux/slices/settingsSlice';
 import { NoteItem } from '@/types';
-import { DragEvent } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { DragEvent, useState, MouseEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 type ItemProps = {
@@ -13,8 +14,18 @@ type ItemProps = {
 const NoteElement: React.FC<ItemProps> = ({ note }) => {
     const dispatch = useDispatch();
     const { activeUserSelection } = useSelector(getSettings);
+    const [actionVisibility, setActionVisibility] = useState<boolean>(false);
 
-    const handleClick = () => {
+    const handleMouseEnter = () => {
+        setActionVisibility(true);
+    };
+
+    const handleMouseLeave = () => {
+        setActionVisibility(false);
+    };
+
+    const handleClick = (e: MouseEvent<HTMLElement>) => {
+        e.stopPropagation();
         dispatch(updateUserSelection(note.id));
         dispatch(updateActiveNote(note.id));
         dispatch(updateActiveFolder(note.folderId));
@@ -36,8 +47,10 @@ const NoteElement: React.FC<ItemProps> = ({ note }) => {
                     : 'text-dark-600 dark:text-dark-400 hover:bg-dark-50 dark:hover:bg-dark-900 hover:text-dark-900 dark:hover:text-dark-200'
             }  ${
                 note.folderId !== DEFAULT_FOLDER.NAME && 'ml-5'
-            } group flex items-center justify-between px-2 py-2 lg:text-sm text-base font-medium rounded-md cursor-pointer`}
+            } group flex items-center justify-between px-2 py-2 lg:text-sm text-base font-medium rounded-md cursor-pointer relative`}
             onClick={handleClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
             draggable
             onDragStart={handleDragStart}
             onDragOver={handleDragOver}
@@ -47,7 +60,7 @@ const NoteElement: React.FC<ItemProps> = ({ note }) => {
                 className={`${
                     activeUserSelection === note?.id
                         ? 'text-blue-300 dark:text-blue-400'
-                        : '  group-hover:text-dark-500 dark:group-hover:text-dark-200'
+                        : 'group-hover:text-dark-500 dark:group-hover:text-dark-200'
                 } mr-4 lg:mr-3 h-5 w-5`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -62,6 +75,96 @@ const NoteElement: React.FC<ItemProps> = ({ note }) => {
                 />
             </svg>
             <span className="flex-grow select-none">{note?.name}</span>
+            {actionVisibility && (
+                <Menu>
+                    {({ open }) => (
+                        <>
+                            <Menu.Button
+                                className="items-end rounded-md focus:outline-none"
+                                onClick={(e: MouseEvent<HTMLButtonElement>) => e.stopPropagation()}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    className="w-5 h-5 text-blue-500"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                                    />
+                                </svg>
+                            </Menu.Button>
+                            <Transition
+                                show={open}
+                                enter="transition ease-out duration-100"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <Menu.Items
+                                    static
+                                    className="absolute right-0 w-32 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-600 rounded-md shadow-lg outline-none dark:border-gray-800 dark:bg-dark-700"
+                                >
+                                    <Menu.Item>
+                                        <span
+                                            className={`text-gray-700 dark:text-gray-400 flex space-x-4 items-center w-full px-4 py-2 text-xs leading-5 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-300 focus:outline-none`}
+                                            onClick={(e: MouseEvent<HTMLButtonElement>) =>
+                                                e.stopPropagation()
+                                            }
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                className="w-4 h-4"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                                                />
+                                            </svg>
+                                            <span>Edit</span>
+                                        </span>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <span
+                                            className={`text-gray-700 dark:text-gray-400 flex space-x-4 items-center w-full px-4 py-2 text-xs leading-5 hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-gray-300 focus:outline-none`}
+                                            onClick={(e: MouseEvent<HTMLButtonElement>) =>
+                                                e.stopPropagation()
+                                            }
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                className="w-4 h-4"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                                />
+                                            </svg>
+                                            <span>Delete</span>
+                                        </span>
+                                    </Menu.Item>
+                                </Menu.Items>
+                            </Transition>
+                        </>
+                    )}
+                </Menu>
+            )}
         </li>
     );
 };
